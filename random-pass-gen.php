7a5b3a654dc7ccff995994ae2bf78218f4b3a4f9
Ashek Ullah <79744460+ullahashek@users.noreply.github.com>
2022-05-14 00:21:31 +0600

    Update random-pass-gen.php

--- random-pass-gen.php
+++ random-pass-gen.php
@@ -1,16 +1,18 @@
 <?php
+    //All Letters And symbols on keyboard
     $all_keys = array('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0','`','~','!','@','#','$','%','^','&','*','(',')','_','-','=','+','[',']','{','}',':',';','|','<','>',',','.','?',' ');
-
-    $length = array(6, 7, 8, 9, 10, 11, 12, 13, 14, 15 );
+    
+    $length = array(6, 7, 8, 9, 10, 11, 12, 13, 14, 15 );//Password Length
     $str = '';
 
     shuffle($length);
-    echo $final_length = $length[0];echo "<br>";
+    $final_length = $length[0];
 
     for ($i=0; $i <$final_length ; $i++) { 
         shuffle($all_keys);
         $str .= $all_keys[0];
     }
+    //Your Password is Ready
     echo "Your Pass is $str";
-        echo "<br>";
+      
 ?>
